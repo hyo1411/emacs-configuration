dff69a2a916856b07cf49345260c4b60c1fcc343
Binh Dao <bdao@safetrust.com>
2016-12-25 12:06:11 +0700

    Update font

--- customizations/60-custom.el
+++ customizations/60-custom.el
@@ -27,8 +27,14 @@
  '(magit-set-upstream-on-push (quote askifnotset))
  '(make-backup-files nil)
  '(projectile-global-mode t)
- )
+ '(show-paren-mode t))
 (provide '60-custom)
 
 ;;; 60-custom.el ends here
 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 100 :width normal)))))
